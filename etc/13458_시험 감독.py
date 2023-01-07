c53ec8a6313b6081487154ae3b0d6746d131f604
dusdjhyeon <dusdj0813@gmail.com>
2023-01-07 11:19:32 +0900

    2023년 1월 7일

---
+++ etc/13458_시험 감독.py
@@ -0,0 +1,17 @@
+#13458 - 시험 감독
+
+import sys
+import math
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().rstrip().split()))
+b,c = map(int, input().rstrip().split())
+director = 0
+
+for i in a:
+    i-=b
+    if i>0:
+        director += math.ceil(i/c)
+    director+=1
+print(director)
